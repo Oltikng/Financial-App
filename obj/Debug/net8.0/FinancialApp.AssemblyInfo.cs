762441711526d9fc0b75fc795c29742070892441
Oltikng <olti.kenga@stud.th-deg.de>
2024-11-15 16:30:08 +0100

    Financial App

--- obj/Debug/net8.0/FinancialApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/FinancialApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinancialApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d348cd60ec4fa4dff8e86319eb86b396e5b46cb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinancialApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinancialApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
